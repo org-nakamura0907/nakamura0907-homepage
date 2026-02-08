c2d5b74e199e0d2197ddf1cc62573a9fdbd08a4d
nakamura0907 <53244048+nakamura0907@users.noreply.github.com>
2026-02-08 23:27:57 +0900

    add: large typography

--- src/shared/components/typography.tsx
+++ src/shared/components/typography.tsx
@@ -17,11 +17,15 @@ function TypographyH1({ className, ...props }: ComponentProps<'h1'>) {
 function TypographyP({ className, ...props }: ComponentProps<'p'>) {
   return <p className={cn('leading-7 [&:not(:first-child)]:mt-6', className)} {...props} />;
 }
+function TypographyLarge({ className, ...props }: ComponentProps<'div'>) {
+  return <div className={cn('text-lg font-semibold', className)} {...props} />;
+}
 function TypographySmall({ className, ...props }: ComponentProps<'small'>) {
   return <small className={cn('text-sm leading-none font-medium', className)} {...props} />;
 }
 
 export const Typography = Object.assign(TypographyP, {
   H1: TypographyH1,
+  Large: TypographyLarge,
   Small: TypographySmall,
 });
